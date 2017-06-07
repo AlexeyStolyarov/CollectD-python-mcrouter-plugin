cc7167da2f138d119e51d68bc3c83dd524dd9d1c
Столяров Алексей <stolyarov@info74.ru>
2017-06-07 14:35:57 +0500

    first version that seems works

---
+++ collectd_tools/collectd_mockup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+
+class Values:
+    plugin = ''
+    interval = ''
+    plugin_instance = ''
+    type = ''
+    type_instance = ''
+    values = ''
+
+    def dispatch(self):
+        print "[plugin_name/plugin_instance/type/type_instance]: %s/%s/%s/%s =  %s" % (
+            self.plugin, self.plugin_instance, self.type, self.type_instance, self.values)
+
+
+class Config:
+    parent = None
+    key = ''
+    values = ()
+    children = ()
+
+    def __init__(self, parent=None, key='', values=(), children=()):
+        self.parent = parent
+        self.key = key
+        self.values = values
+        self.children = children
+
+
+def debug(arg):
+    print '[DEBUG] %s' % arg
+
+
+def info(arg):
+    print '[INFO] %s' % arg
+
+
+def warning(arg):
+    print '[WARNING] %s' % arg
+
+
+def register_read(read_callback):
+    read_callback()
